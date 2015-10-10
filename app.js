24b1262aee2a913970a6ec2f164049566f17ca56
Lisa Jian <lisajian@berkeley.edu>
2015-10-10 03:23:32 -0700

    Made progress on navbar

--- app.js
+++ app.js
@@ -1,20 +1,65 @@
-var myDataRef = new Firebase('https://blinding-inferno-7599.firebaseio.com/');
-
-$('#messageInput').keypress(function (e) {
-  if (e.keyCode == 13) {
-    var name = $('#nameInput').val();
-    var text = $('#messageInput').val();
-    myDataRef.push({name: name, text: text});
-    $('#messageInput').val('');
-  }
+var ref = new Firebase('https://blinding-inferno-7599.firebaseio.com/');
+​
+/* Triggers login attempt with name, text.
+ * NOTE: it only prints messages now
+ *   
+*/
+$('#loginButton').click(function () {
+  var authData = ref.getAuth();
+  var email = $('#loginEmailInput').val();
+  var password = $('#loginPassInput').val();
+​
+  ref.authWithPassword({
+    email   : email,
+    password: password
+  }, authHandler);
+  $('#loginPassInput').val('');
 });
-
+​
+/* Creates a new user given 
+*/
+$('#signUpButton').click(function() {
+  var email = $('#emailInput').val();
+  var password = $('#passwordInput1').val();
+  var confirm_password = $('#passwordInput2').val();
+​
+  if (password != confirm_password){
+    console.log("Password does not match");
+  } else {
+    ref.createUser({
+      email    : email,
+      password : password
+    }, function(error, userData) {
+      if (error) {
+        console.log("Error creating user:", error);
+      } else {
+        console.log("Successfully created user account with uid:", userData.uid);
+      }
+    });
+  }
+  $('#passwordInput1').val('');
+  $('#passwordInput2').val('');
+});
+​
+/*
 myDataRef.on('child_added', function(snapshot) {
   var message = snapshot.val();
   displayChatMessage(message.name, message.text);
 });
-
+*/
+​
 function displayChatMessage(name, text) {
   $('<div/>').text(text).prepend($('<em/>').text(name+': ')).appendTo($('#messagesDiv'));
   $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
 };
+​
+/* Handler to check authentication 
+*/
+function authHandler(error, authData) {
+  if (error) {
+    console.log("Login Failed!", error);
+  } else {
+    console.log("Authenticated successfully with payload:", authData);
+  }
+}
+​
